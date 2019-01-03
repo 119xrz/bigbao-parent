bbcf7e7b65e988d45a788ddfcaffc9d8fc3aeb46
bigbao <lizhenbin@ccx.cn>
2019-01-03 14:56:36 +0800

    add data

---
+++ bigbao-data/src/test/java/com/bigbao/data/BigbaoDataApplicationTests.java
@@ -0,0 +1,17 @@
+package com.bigbao.data;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BigbaoDataApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
--- bigbao-graph/src/main/java/com/bigbao/graph/config/RedisConfig.java
+++ bigbao-graph/src/main/java/com/bigbao/graph/config/RedisConfig.java
@@ -1,21 +1,15 @@
 package com.bigbao.graph.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.core.env.MapPropertySource;
-import org.springframework.data.redis.connection.RedisClusterConfiguration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @Date 2018/8/31
